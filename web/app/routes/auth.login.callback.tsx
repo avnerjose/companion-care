6f63217ed1e1e4fcb702a6661943326a7fee9953
Avner José <avnerj.g.r@gmail.com>
2023-12-18 09:38:11 -0300

    feat: implementing log on error

--- web/app/routes/auth.login.callback.tsx
+++ web/app/routes/auth.login.callback.tsx
@@ -1,17 +1,21 @@
-import { type LoaderArgs } from "@remix-run/node";
-import { createUserSession, verifyLogin } from "../utils/session.server.ts";
+import { type LoaderArgs } from '@remix-run/node';
+import { createUserSession, verifyLogin } from '../utils/session.server.ts';
 
 export async function loader({ request }: LoaderArgs) {
-  const url = new URL(request.url);
-  const queryParams = new URLSearchParams(url.search);
+  try {
+    const url = new URL(request.url);
+    const queryParams = new URLSearchParams(url.search);
 
-  const token = queryParams.get("token");
-  const userSession = await verifyLogin(token as string);
+    const token = queryParams.get('token');
+    const userSession = await verifyLogin(token as string);
 
-  return createUserSession({
-    request,
-    userSession,
-  });
+    return createUserSession({
+      request,
+      userSession,
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 export default function AuthLoginCallback() {}
