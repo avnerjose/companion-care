cdf65fddc833d2c6209102a61ba3c5a887ba211d
Avner José <avnerj.g.r@gmail.com>
2023-12-26 15:02:48 -0300

    feat: creting gratient on login page

--- web/src/app/(auth)/layout.tsx
+++ web/src/app/(auth)/layout.tsx
@@ -1,12 +1,20 @@
 import { PropsWithChildren } from "react";
 import Image from "next/image";
 import Logo from "@/assets/companion_care_logo.png";
+import Illustration from "@/assets/hospital_people.png";
+import Dots from "@/assets/dots.svg";
 import CompanyIcon from "@/assets/icons/logo.svg";
 
 export default function LoginLayout({ children }: PropsWithChildren) {
   return (
-    <main className="grid bg-secondary-700 h-screen w-screen grid-cols-[60%,1fr] max-md:grid-cols-[1fr,2fr] max-sm:flex max-sm:px-4 max-sm:pt-4 overflow-hidden">
-      <div className="bg-secondary-700 flex h-full w-full items-end justify-center max-sm:hidden" />
+    <main className="grid bg-primary-500 h-screen w-screen grid-cols-[60%,1fr] max-md:grid-cols-[1fr,2fr] max-sm:flex max-sm:px-4 max-sm:pt-4 overflow-hidden">
+      <div className="bg-gradient-to-r from-secondary-700 via-secondary-500 via-30% to-primary-500 flex relative h-full w-full items-end justify-center max-sm:hidden">
+        <Image
+          src={Illustration}
+          className="w-[70%]"
+          alt="Hospital illustration"
+        />
+      </div>
       <div className="bg-white rounded-tl-2xl max-sm:rounded-t-2xl overflow-y-auto overflow-x-hidden w-full p-5 ">
         <div className="flex flex-col mx-auto items-start justify-center gap-4 bg-white ">
           <div className="mb-5 flex items-center gap-2 mt-4">
@@ -15,7 +23,11 @@ export default function LoginLayout({ children }: PropsWithChildren) {
               className="h-10 w-10"
               alt="CompanionCare"
             />
-            <Image src={Logo} alt="CompanionCare" className="w-64 max-sm:w-[80%]" />
+            <Image
+              src={Logo}
+              alt="CompanionCare"
+              className="w-64 max-sm:w-[80%]"
+            />
           </div>
           {children}
         </div>
